96e662737358876e63e85eabefba5c611e659171
Alejandra Avalos Pacheco <alejandraavalospacheco@MacBook-Pro-de-Alejandra.local>
2018-11-05 19:17:13 +0000

    Adding batch effects to the documentation examples

--- examples/example.R
+++ examples/example.R
@@ -1,6 +1,8 @@
-n<-100
-p<-1000
+n<-200
+p<-250
 q<-10
+Pv = 1
+Pb = 2
 
 grid<-seq(-1,1,length.out=p)
 M<-matrix(grid,ncol=q,nrow=p)
@@ -9,8 +11,20 @@ for(i in 2:q){
   M[,i]<-grid[c((i*rate):p,1:(i*rate-1))]
 }
 
-E <- rmvnorm(n,numeric(p),diag(p))
 Z <- rmvnorm(n,numeric(q),diag(q))
-X <- Z%*%t(M) + E
+Psi <- diag(c(rep(.5,p)),p)
+V <- matrix(ncol = Pv, nrow = n, runif(n*Pv,0,3))
+Theta = matrix(ncol=Pv,c(rep(-2,round(p/2)),rep(2,p-round(p/2))))
+batch = round(runif(n,0,1),0)
+B = matrix(ncol=Pb, c(batch,ifelse(batch==1,0,1)))
+Beta = matrix(ncol=Pb, c(rep(2,p),rep(0,p)))
+Tau_inv = cbind(diag(Psi),diag(Psi)*1.5)
+Er = matrix(ncol=p, nrow=n)
+for (i in 1:n){
+  if(i%%10==0){print(i)}
+  Er[i,] = mvrnorm(1, rep(0,p), diag(c(Tau_inv %*% B[i,]),p), tol = 1e-6, empirical = FALSE, EISPACK = FALSE)
+}
 
-result=BFR.BE.EM(x=X, q = 10,varianceBE=FALSE)
+X <- Z%*%t(M) + V%*%t(Theta) + B%*%t(Beta) + Er
+
+result=BFR.BE.EM(x=X,v=V,b=B,q = 10)
